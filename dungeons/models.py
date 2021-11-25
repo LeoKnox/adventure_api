4acad0136b66a1c7fa325e968873b5348d721a55
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-25 08:59:31 -0800

    Add files via upload

--- dungeons/models.py
+++ dungeons/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Level(models.Model):
     level_name = models.CharField(max_length=100, blank=False)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.level_name}: {self.description}"
