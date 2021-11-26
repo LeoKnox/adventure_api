28fa79f934b9611a431a986713ae229d8de4f038
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-26 13:33:01 -0800

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Room(models.Model):
     room_name = models.CharField(max_length=200, blank=False)
     description = models.TextField(blank=True)
-    width = models.IntgerField(blank=False, default=5)
-    length = models.IntgerField(blank=False, default=5)
+    width = models.IntegerField(blank=False, default=5)
+    length = models.IntegerField(blank=False, default=5)
+
+    def __str__(self):
+        return f"{self.room_name}: {self.width} by {self.length}"
