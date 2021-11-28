b3250b5ecebdcfb44553e0b816bc0601f9501b9a
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-28 11:45:18 -0800

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.RoomListAPIView.as_view(), name='room_list'),
-    path('<int:id>/', views.RoomRetrieveAPIView.as_view(), name='room_detail'),
-    path('create/', views.RoomCreateAPIView.as_view(), name='room_create'),
-]
