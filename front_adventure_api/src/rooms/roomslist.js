d83b5a904b5e1818b9ed6b22d2b78401711a6016
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-29 12:01:38 -0800

    Add files via upload

---
+++ front_adventure_api/src/rooms/roomslist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import TestData from './test_data.json';
+
+class RoomList extends Component {
+    render() {
+        return(
+            <h5>Room gets printed here</h5>
+        )
+    }
+}
+export default RoomList;
