0bbed5241f51ca05627b680786412919a4d52584
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-29 11:41:14 -0800

    Add files via upload

--- front_adventure_api/src/App.js
+++ front_adventure_api/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Create Adventures using Python API
         </p>
         <a
           className="App-link"
