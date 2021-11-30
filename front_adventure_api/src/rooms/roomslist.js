92e38411fb20503a7e3ccd3f8afacaeb682fd488
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-30 11:40:55 -0800

    Add files via upload

--- front_adventure_api/src/rooms/roomslist.js
+++ front_adventure_api/src/rooms/roomslist.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import TestData from './test_data.json';
-import RoomDetail from './roomdetail';
+import axios from 'axios';
 
 class RoomList extends Component {
+    state = {
+        roomsData:[],
+    }
+
+    componentDidMount(){
+        axios.get("http://127.0.0.1:8000/")
+        .then((response) => {
+            this.setState({roomsData: response.data})
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+
     render() {
         return(
             <div>
-                {TestData.map( item => {
-                    return <RoomDetail r = {item} />
+                {this.state.roomsData.map( item => {
+                    return <h3>{item.room_name}: {item.description}</h3>
                     })
                 }
             </div>
