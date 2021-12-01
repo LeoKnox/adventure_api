42e42e0aad372543adc914758fdf18fd9169500c
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-01 10:53:46 -0800

    Add files via upload

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Room
+from rest_framework.reverse import reverse
 
 class RoomListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,7 +9,11 @@ class RoomListSerializer(serializers.ModelSerializer):
             'id',
             'room_name',
             'description',
+            'absolute',
         ]
+    def get_absolute(self, obj):
+        return reverse('room_detail', arg=(obj.pk,))
+
 class RoomDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
