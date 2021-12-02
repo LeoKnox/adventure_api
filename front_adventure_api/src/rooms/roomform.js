73b560f7d9f001d0d9711a760e77eff88fc5cd10
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-02 11:22:39 -0800

    Add files via upload

--- front_adventure_api/src/rooms/roomform.js
+++ front_adventure_api/src/rooms/roomform.js
@@ -2,7 +2,39 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class RoomForm extends React.Component {
-    //Code
+    constructor(props) {
+        super(props);
+        this.state = {
+            room_name: " ",
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+}
+
+handleChange(event) {
+    //code
+}
+
+handleSubmit(event) {
+    event.preventDefault();
+}
+
+render() {
+    const { room_name } = this.state;
+    return (
+        <form onSubmit={this.handleSubmit}>
+            <div>
+                <input
+                    type="text"
+                    name="room_name"
+                    value={room_name}
+                    onChange={this.handleChange}
+                />
+            </div>
+            <input type="submit" value="Submit" />
+        </form>
+    )
 }
 
 export default RoomForm;
