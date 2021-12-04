d81af2bf8049d35907f5cdd7ced45fb1515570f6
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-03 22:10:33 -0800

    Add files via upload

--- front_adventure_api/src/rooms/roomdetail.js
+++ front_adventure_api/src/rooms/roomdetail.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 
 class RoomDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showComponent: false,
+        };
+        this.updateRoomDetails = this.updateRoomDetails.bind(this);
+    }
+    updateRoomDetails() {
+        this.setState({ showComponent: true });
+    }
     render() {
         const obj = this.props.roomDetail;
         return (
             <div style={{ color: "yellow", border: "1px solid yellow" }}>
                 <h4>{obj.room_name}</h4>
                 <p>{obj.description}</p>
+                <button
+                    style={{ backgroundColor: "white" }}
+                    onClick={() => this.updateRoomDetails()}
+                >
+                    Update
+                </button>
+                {this.state.showComponent ? <RoomUpdate roomUpdate={obj} />: null}
             </div>
         );
     }
